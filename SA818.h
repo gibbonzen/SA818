420d477d5dc171313f430bb006cfd573ca22417b
Gibbon <julian.mauborgne@gmail.com>
2022-12-09 11:04:19 +0100

    Working code

--- SA818.h
+++ SA818.h
@@ -1,8 +1,6 @@
 #ifndef SA818_H
 #define SA818_H
 
-#include <Stream.h>
-
 class Request {
     public:
         char* req;
@@ -22,13 +20,11 @@ enum Type { SA_818 = 0, SA_868 = 1 };
 
 class SA818 {
     private:
-        byte type_ = Type::SA_818;
         bool debug_ = false;
         Stream* stream_ = 0;
         long timeout_ = 1000;
 
         Response lastResponse_;
-        byte bw_;
 
         bool request(Request*);
         void print(char*);
@@ -38,33 +34,15 @@ class SA818 {
         bool waitResponse(Response*);
         bool success(Response*);
 
-        String ctcss(int);
-
-        float loopScan(float, bool, float = 0);
-
     public:
         SA818(Stream*);
         ~SA818();
 
-        void set(Type);
         void debug(bool);
         void timeout(long = 1000);
 
         bool send(char*, int = 0, char** = 0);
-        char* response();
-
-        bool connect();
-        bool setGroup(byte, float, float, int, byte, int);
-        bool scan(float);
-        bool setVolume(int);
-        bool setFilter(int, int, int);
-        bool openTail();
-        bool closeTail();
-        String rssi();
-        String version();
-
-        float next(float, float = 0);
-        float previous(float, float = 0);
+        Response* response();
 };
 
 #endif
@@ -74,10 +52,6 @@ SA818::SA818(Stream* stream) : stream_(stream) {}
 SA818::~SA818() {
     stream_ = 0;
     lastResponse_.req = 0;
-}
-
-void SA818::set(Type type) {
-    type_ = type;
 }
 
 void SA818::debug(bool deb) {
@@ -202,125 +176,6 @@ bool SA818::success(Response* resp) {
     return false;
 }
 
-char* SA818::response() {
-    return lastResponse_.raw.c_str();
+Response* SA818::response() {
+    return &lastResponse_;
 }
-
-String SA818::ctcss(int sub) {
-    String ssub = String(abs(sub));
-    if(sub < 0)
-        ssub += "N";
-    if(sub >= 23)
-        ssub += "I";
-
-    String res = ssub;
-    for (int i = ssub.length(); i < 4; i++) {
-        res = "0" + res;
-    }
-    return String(res);
-}
-
-bool SA818::connect() {
-    return send("AT+DMOCONNECT");
-}
-
-bool SA818::setGroup(byte bw, float tx_f, float rx_f, int tx_sub, byte sq, int rx_sub) {
-    bw_ = bw;
-
-    String b = String(bw);
-    String tf = String(tx_f, 4);
-    String rf = String(rx_f, 4);
-    String tsub = ctcss(tx_sub);
-    String s = String(sq);
-    String rsub = ctcss(rx_sub);
-    char* params[] = {
-        b.c_str(),
-        tf.c_str(),
-        rf.c_str(),
-        tsub.c_str(),
-        s.c_str(),
-        rsub.c_str()
-    };
-    return send("AT+DMOSETGROUP", 6, params);
-}
-
-bool SA818::scan(float freq) {
-    String cmd = String("S+") + String(freq, 4);
-    return send(cmd.c_str());
-}
-
-bool SA818::setVolume(int vol) {
-    String v =  String(vol);
-    char* params[] = { v.c_str() };
-    return send("AT+DMOSETVOLUME", 1, params);
-}
-
-bool SA818::setFilter(int emph, int high, int low) {
-    String e = String(emph);
-    String h = String(high);
-    String l = String(low);
-    char* params[] = { e.c_str(), h.c_str(), l.c_str() };
-    return send("AT+SETFILTER", 3, params);
-}
-
-bool SA818::openTail() {
-    return send("AT+SETTAIL=1");
-}
-
-bool SA818::closeTail() {
-    return send("AT+SETTAIL=0");
-}
-
-String SA818::rssi() {
-    String cmd = "RSSI?";
-    if(type_ == SA_868)
-        cmd = "AT+RSSI?";
-
-    if(send(cmd.c_str()))
-        return lastResponse_.res;
-    return response();
-}
-
-
-String SA818::version() {
-    if(send("AT+VERSION"))
-        return lastResponse_.res;
-    return response();
-}
-
-float SA818::loopScan(float mhz, bool dir, float khz) {
-    // | GHz |     |     | MHz ||     |     | KHz ||     |     | Hz |
-    // |     |     |     |     ||     |     |   1 ||   0 |   0 |  0 |
-    // |     |     |     |   1 ||   0 |   0 |   0 ||   0 |   0 |  0 |
-    // |   1 |   0 |   0 |   0 ||   0 |   0 |   0 ||   0 |   0 |  0 |
-    // |     |   1 |   4 |   5 ||   7 |   5 |   0 ||   0 |   0 |  0 |
-    // |     |     |     |     ||     |   1 |   2 ||   5 |   0 |  0 |
-    
-    float f = mhz * 1000; // MHz to KHz
-
-    if(khz == 0) {
-        khz = 12.5; // 12.5K
-
-        if(bw_ == 1) // 25K
-            khz = 25;
-    }
-
-    if(dir) f = f + khz;
-    else f = f - khz;
-
-    f = f / 1000;
-
-    if(scan(f)) return f;
-    else return loopScan(f, dir);
-}
-
-float SA818::next(float mhz, float khz) {
-    return loopScan(mhz, true, khz);
-}
-
-float SA818::previous(float mhz, float khz) {
-    return loopScan(mhz, false, khz);
-}
-
-/////////////////////////////////////////////////
-
