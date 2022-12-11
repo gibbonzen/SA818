8d6a5ae391bebeeceda676e6f609717949914b80
Gibbon <julian.mauborgne@gmail.com>
2022-12-11 19:49:32 +0100

    bug fix & refacto scanLoop()

--- SA818Controller.h
+++ SA818Controller.h
@@ -2,53 +2,53 @@
 #define SA818_CONTROLLER_H
 
 class SA818Controller {
-    private:
-        SA818* sa = 0;
-        byte type_ = Model::SA_818;
-        byte band_ = Band::VHF;
+private:
+    SA818* sa = 0;
+    byte type_ = Model::SA_818;
+    byte band_ = Band::VHF;
 
-        byte bw_;
-        float tx_f_;
-        float rx_f_;
-        int tx_sub_;
-        byte sq_;
-        int rx_sub_;
+    byte bw_;
+    float tx_f_;
+    float rx_f_;
+    int tx_sub_;
+    byte sq_;
+    int rx_sub_;
 
-        bool scanning = false;
+    bool scanning = false;
 
-        String ctcss(int);
-        float loopScan(float, bool, long = millis(), float = 0, long = 60000);
+    String ctcss(int);
+    float loopScan(float, bool, float = 0, unsigned long = 0);
 
-    public:
-        SA818Controller(SA818*);
-        ~SA818Controller();
-        void setModel(Model = Model::SA_818);
-        void setBand(Band = Band::VHF);
-        String response();
-        String result();
+public:
+    SA818Controller(SA818*);
+    ~SA818Controller();
+    void setModel(Model = Model::SA_818);
+    void setBand(Band = Band::VHF);
+    String response();
+    String result();
 
-        void setBW(byte);
-        void setTXF(float);
-        void setRXF(float);
-        void setTXSub(int);
-        void setSQ(byte);
-        void setRXSub(int);
-        bool update();
+    void setBW(byte);
+    void setTXF(float);
+    void setRXF(float);
+    void setTXSub(int);
+    void setSQ(byte);
+    void setRXSub(int);
+    bool update();
 
-        bool connect();
-        bool setGroup(byte, float, float, int, byte, int);
-        bool scan(float);
-        bool setVolume(byte);
-        bool setFilter(byte, byte, byte);
-        bool openTail();
-        bool closeTail();
-        String rssi();
-        String version();
+    bool connect();
+    bool setGroup(byte, float, float, int, byte, int);
+    bool scan(float);
+    bool setVolume(byte);
+    bool setFilter(byte, byte, byte);
+    bool openTail();
+    bool closeTail();
+    String rssi();
+    String version();
 
-        float next(float, float = 0, long = 60000);
-        float previous(float, float = 0, long = 60000);
-        int scanlist(float[], int, float[]);
-        void stopScan();
+    float next(float, float = 0, unsigned long = 0);
+    float previous(float, float = 0, unsigned long = 0);
+    int scanlist(float[], int, float[]);
+    void stopScan();
 };
 
 #endif
@@ -105,9 +105,9 @@ bool SA818Controller::update() {
 
 String SA818Controller::ctcss(int sub) {
     String ssub = String(abs(sub));
-    if(sub < 0)
+    if (sub < 0)
         ssub += "N";
-    if(sub >= 23)
+    if (sub >= 23)
         ssub += "I";
 
     String res = ssub;
@@ -154,7 +154,7 @@ bool SA818Controller::scan(float freq) {
 }
 
 bool SA818Controller::setVolume(byte vol) {
-    String v =  String(vol);
+    String v = String(vol);
     char* params[] = { v.c_str() };
     return sa->send("AT+DMOSETVOLUME", 1, params);
 }
@@ -177,22 +177,22 @@ bool SA818Controller::closeTail() {
 
 String SA818Controller::rssi() {
     String cmd = "RSSI?";
-    if(type_ == Model::SA_868)
+    if (type_ == Model::SA_868)
         cmd = "AT+RSSI?";
 
-    if(sa->send(cmd.c_str()))
+    if (sa->send(cmd.c_str()))
         return sa->response()->res;
     return sa->response()->raw;
 }
 
 
 String SA818Controller::version() {
-    if(sa->send("AT+VERSION"))
+    if (sa->send("AT+VERSION"))
         return sa->response()->res;
     return sa->response()->raw;
 }
 
-float SA818Controller::loopScan(float mhz, bool dir, long timer, float stop, long timeout) {
+float SA818Controller::loopScan(float mhz, bool dir, float stop, unsigned long timeout) {
     // | GHz |     |     | MHz ||     |     | KHz ||     |     | Hz |
     // |     |     |     |     ||     |     |   1 ||   0 |   0 |  0 |
     // |     |     |     |   1 ||   0 |   0 |   0 ||   0 |   0 |  0 |
@@ -200,59 +200,68 @@ float SA818Controller::loopScan(float mhz, bool dir, long timer, float stop, lon
     // |     |   1 |   4 |   5 ||   7 |   5 |   0 ||   0 |   0 |  0 |
     // |     |     |     |     ||     |   1 |   2 ||   5 |   0 |  0 |
 
-    if(stop == 0)
+    if (stop == 0)
         stop = mhz;
 
-    float f = mhz * 1000; // MHz to KHz
-
     float khz = 12.5; // 12.5K
-    if(bw_ == 1) 
+    if (bw_ == 1)
         khz = 25; // 25K
-
-    if(dir) f = f + khz;
-    else f = f - khz;
 
     // Out of frequencies bounds
     float top = 174000; // VHF KHz top limit
     float bottom = 134000;
-    if(band_ == Band::UHF) {
+    if (band_ == Band::UHF) {
         top = 480000; // UHF
         bottom = 400000;
     }
 
-    if (dir) {
-        f = f + khz;
+    unsigned long timer = millis();
+    float f = mhz;
+    bool next = true;
+    while (next) {
+        f *= 1000; // MHz to KHz
 
-        if (f > top) f = bottom; // out of top bound
+        if (dir) {
+            f = f + khz;
+
+            if (f > top) f = bottom; // out of top bound
+        }
+        else {
+            f = f - khz;
+
+            if (f < bottom) f = top; // out of bottom bound
+        }
+        f /= 1000;
+
+        next = !scan(f);
+
+        if(timeout != 0 && millis() - timer >= timeout) {
+            next = false;
+        }
+        
+        if (f == stop) {
+            next = false;
+        }
     }
-    else {
-        f = f - khz;
-
-        if (f < bottom) f = top; // out of bottom bound
-    }
-    f = f / 1000;
-
-    if(scan(f)) return f;
-    if(f == stop || millis() - timer >= timeout) return mhz;
-    else return loopScan(f, dir, timer, stop, timeout);
+    return f;
 }
 
-float SA818Controller::next(float mhz, float stop, long timeout) {
-    if(stop == 0)
+float SA818Controller::next(float mhz, float stop, unsigned long timeout) {
+    if (stop == 0)
         stop = mhz;
-    return loopScan(mhz, true, millis(), stop, timeout);
+    return loopScan(mhz, true, stop, timeout);
 }
 
-float SA818Controller::previous(float mhz, float stop, long timeout) {
-    if(stop == 0)
+float SA818Controller::previous(float mhz, float stop, unsigned long timeout) {
+    if (stop == 0)
         stop = mhz;
-    return loopScan(mhz, false, millis(), stop, timeout);
+    return loopScan(mhz, false, stop, timeout);
 }
 
 int SA818Controller::scanlist(float list[], int size, float ret[]) {
     int r = 0;
-    for(int i = 0; i < size; i++) {
-       if(scan(list[i])) 
+    for (int i = 0; i < size; i++) {
+        if (scan(list[i]))
             ret[r++] = list[i];
     }
 
